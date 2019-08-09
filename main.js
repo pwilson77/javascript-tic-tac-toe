af35b16bb9c8e0c108f6051f58a859fe80773735
pwilson77 <p.o.yirenkyi@gmail.com>
2019-08-09 16:00:45 +0000

    setting up eslint locally

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-"use strict";
-
 const Gameboard = (() => {
   const gameBoardArray = [];
 })();
 
-const displayController = (() => {})();
+const displayController = (() => { })();
 
-const Player = name => {
+const Player = (name) => {
   const getName = () => name;
   let playerArray = [];
 
-  const newPosition = pos => {
+  const newPosition = (pos) => {
     playerArray.push(pos);
   };
 
