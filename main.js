58bb6a428306f8eb3586295fe589834065f759e5
pwilson77 <p.o.yirenkyi@gmail.com>
2019-08-10 15:57:54 +0000

    if implemented the game start screen will be seen

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
-const Gameboard = (() => {
+const gameboard = (() => {
   const gameBoardArray = [];
 })();
 
 const displayController = (() => { })();
 
-const gameCycleController = (() => { })();
-
-const Player = (name) => {
+const player = (name, sym) => {
   const getName = () => name;
-  let playerArray = [];
+  const getSymbol = () => sym;
+  const playerArray = [];
 
   const addPosition = (pos) => {
     playerArray.push(pos);
   };
-
   return { addPosition };
 };
+
+const gameCycleController = (() => {
+  let player1;
+  let player2;
+  let currentPlayer;
+  const gameOver = () => {
+
+  };
+
+  const switchPlayer = (curPlayer) => {
+    switch (curPlayer) {
+      case player1:
+        currentPlayer = player2;
+        break;
+      case player2:
+        currentPlayer = player1;
+        break;
+      default:
+        currentPlayer = player1;
+    }
+  };
+
+  const gameStart = () => {
+    const firstname = document.getElementById('player1');
+    const secondname = document.getElementById('player2');
+    player1 = player(firstname, 'X');
+    player2 = player(secondname, 'O');
+    const form = document.getElementById('form');
+    form.style.display = 'none';
+    const board = document.getElementById('container');
+    board.style.display = 'flex';
+    while (!gameOver) {
+      switchPlayer(currentPlayer);
+    }
+  };
+  return {
+    gameStart,
+  };
+})();
