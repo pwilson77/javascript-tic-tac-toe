a47a67c25407e91820d34c39bf4a42d164a7f734
Oghenebrume50 <raphlbrume@gmail.com>
2019-08-12 14:51:23 +0100

    make players play with alternating values showing

--- main.js
+++ main.js
@@ -12,23 +12,25 @@ const player = (name, sym) => {
   const addPosition = (pos) => {
     playerArray.push(pos);
   };
-  return { addPosition };
+  return { addPosition, getSymbol };
 };
 
 const gameCycleController = (() => {
   let player1;
   let player2;
   let currentPlayer;
+  let count = 1;
   const gameOver = () => {
-
+    return false
   };
 
   const switchPlayer = (curPlayer) => {
+    console.log(`${curPlayer} that is the current player`);
     switch (curPlayer) {
-      case player1:
+      case 0:
         currentPlayer = player2;
         break;
-      case player2:
+      case 1:
         currentPlayer = player1;
         break;
       default:
@@ -36,7 +38,22 @@ const gameCycleController = (() => {
     }
   };
 
+  function gameLoop(boxId) {
+    console.log(boxId+" the this id");
+    const box = document.getElementById(boxId);
+    box.removeAttribute()
+    console.log(`${box.innerText} beforee againnn`);
+    const detectPlayer = count % 2;
+
+    switchPlayer(detectPlayer);
+    const symbol = currentPlayer.getSymbol();
+    box.innerHTML = symbol;
+    console.log(`${box.innerText} ${symbol} and againnn`);
+    count++;
+  }
+
   const gameStart = () => {
+    console.log('start now');
     const firstname = document.getElementById('player1');
     const secondname = document.getElementById('player2');
     player1 = player(firstname, 'X');
@@ -45,11 +62,9 @@ const gameCycleController = (() => {
     form.style.display = 'none';
     const board = document.getElementById('container');
     board.style.display = 'flex';
-    while (!gameOver) {
-      switchPlayer(currentPlayer);
-    }
   };
   return {
     gameStart,
+    gameLoop,
   };
 })();
