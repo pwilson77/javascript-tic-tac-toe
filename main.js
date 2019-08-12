d62ecafbc9a0144c9f9d5cac310c2d02d88196f1
Oghenebrume50 <raphlbrume@gmail.com>
2019-08-12 17:59:15 +0100

    I have to fix the reset now

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-const gameboard = (() => {
-  const gameBoardArray = [];
-})();
-
-const displayController = (() => { })();
-
 const player = (name, sym) => {
   const getName = () => name;
   const getSymbol = () => sym;
@@ -12,16 +6,88 @@ const player = (name, sym) => {
   const addPosition = (pos) => {
     playerArray.push(pos);
   };
-  return { addPosition, getSymbol };
+  return { addPosition, getSymbol, getName };
 };
 
 const gameCycleController = (() => {
+  const gameBoardArray = [];
+  const statusAlert = document.createElement('h1');
   let player1;
   let player2;
   let currentPlayer;
   let count = 1;
-  const gameOver = () => {
-    return false
+  
+  const reset = () => {
+    gameBoardArray = [];
+    currentPlayer = player1;
+    count = 1;
+  }
+
+  const win = () => {
+    statusAlert.innerText = `${currentPlayer.getName()} won this game`;
+    document.body.appendChild(statusAlert);
+  }
+
+  const draw = () => {
+    statusAlert.innerText = 'Not bad it is a draw';
+    document.body.appendChild(statusAlert);
+  }
+
+  const gameStatus = () => {
+    switch (true) {
+      case gameBoardArray[0] === currentPlayer.getSymbol() && 
+      (gameBoardArray[0] === gameBoardArray[1] && 
+      gameBoardArray[1] === gameBoardArray[2] && 
+      gameBoardArray[0] === gameBoardArray[2]):
+        win();
+        break;
+      case gameBoardArray[3] === currentPlayer.getSymbol() && 
+      (gameBoardArray[3] === gameBoardArray[4] && 
+      gameBoardArray[4] === gameBoardArray[5] && 
+      gameBoardArray[3] === gameBoardArray[5]):
+        win();
+        break;
+      case gameBoardArray[6] === currentPlayer.getSymbol() &&
+      (gameBoardArray[6] === gameBoardArray[7] && 
+      gameBoardArray[7] === gameBoardArray[8] && 
+      gameBoardArray[6] === gameBoardArray[8]):
+        win();
+        break;
+      case gameBoardArray[0] === currentPlayer.getSymbol() && 
+      (gameBoardArray[0] === gameBoardArray[3] && 
+      gameBoardArray[0] === gameBoardArray[6] && 
+      gameBoardArray[3] === gameBoardArray[6]):
+        win();
+        break;
+      case gameBoardArray[1] === currentPlayer.getSymbol() && 
+      (gameBoardArray[1] === gameBoardArray[4] && 
+      gameBoardArray[4] === gameBoardArray[7] && 
+      gameBoardArray[7] === gameBoardArray[1]):
+        win();
+        break;
+      case gameBoardArray[2] === currentPlayer.getSymbol() && 
+      (gameBoardArray[2] === gameBoardArray[5] && 
+      gameBoardArray[5] === gameBoardArray[8] && 
+      gameBoardArray[2] === gameBoardArray[8]):
+        win();
+        break;
+      case gameBoardArray[0] === currentPlayer.getSymbol() && 
+      (gameBoardArray[0] === gameBoardArray[4] && 
+      gameBoardArray[4] === gameBoardArray[8] && 
+      gameBoardArray[0] === gameBoardArray[8]):
+        win();
+        break;
+      case gameBoardArray[2] === currentPlayer.getSymbol() && 
+      (gameBoardArray[2] === gameBoardArray[4] && 
+      gameBoardArray[4] === gameBoardArray[6] && 
+      gameBoardArray[2] === gameBoardArray[6]):
+        win();
+        break;
+      default:
+        if (count >= 9) {
+          draw();
+        }
+    }
   };
 
   const switchPlayer = (curPlayer) => {
@@ -38,26 +104,36 @@ const gameCycleController = (() => {
     }
   };
 
+  const move = (pos, symbol) => {
+    gameBoardArray[pos] = symbol;
+    console.log(gameBoardArray);
+  }
+
+  const renderGameArray = () => {
+    const gameBoard = [...document.querySelectorAll('.gameboard a')];
+    gameBoard.forEach((cell, i) => {
+      cell.innerText = gameBoardArray[i] || '';
+    });
+  }
+
   function gameLoop(boxId) {
-    console.log(boxId+" the this id");
     const box = document.getElementById(boxId);
-    box.removeAttribute()
-    console.log(`${box.innerText} beforee againnn`);
+    box.removeAttribute('onclick');
     const detectPlayer = count % 2;
 
     switchPlayer(detectPlayer);
     const symbol = currentPlayer.getSymbol();
-    box.innerHTML = symbol;
-    console.log(`${box.innerText} ${symbol} and againnn`);
-    count++;
+    move(Number(boxId) - 1, symbol);
+    renderGameArray();
+    gameStatus();
+    count += 1;
   }
 
   const gameStart = () => {
-    console.log('start now');
     const firstname = document.getElementById('player1');
     const secondname = document.getElementById('player2');
-    player1 = player(firstname, 'X');
-    player2 = player(secondname, 'O');
+    player1 = player(firstname.value, 'X');
+    player2 = player(secondname.value, 'O');
     const form = document.getElementById('form');
     form.style.display = 'none';
     const board = document.getElementById('container');
